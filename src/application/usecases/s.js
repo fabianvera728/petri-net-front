3cf6214127fe6d7e9695a6cdf0ec0ead162123b0
FabianVC <fabianvera728@gmail.com>
2022-10-12 03:27:38 -0500

    [fix] Upload resources

---
+++ src/application/usecases/s.js
@@ -0,0 +1,25 @@
+/*
+{
+    places: [
+        {
+            name, id, tokens
+        }
+    ]
+
+    transitions: [
+        {
+            id
+            name,
+            entries: [
+                {
+                    placeId, countEntries
+                }
+            ]
+            outputs: [
+                {
+                    placeId, countOutputs
+                }
+            ]
+        }
+    ]
+}*/
